6e5f87a9fcaac435b57eec1ccd54dea8bafd54d9
marklemay <riteofwhey@gmail.com>
2013-11-08 17:20:30 -0500

    test

--- src/test/Math/MathTest.java
+++ src/test/Math/MathTest.java
@@ -28,34 +28,34 @@ public class MathTest {
         System.out.println("[[ATTACHMENT|" + path + "]]"); 
         System.out.println();
         
-        //with a stupidly absolute path
-        System.out.println();
-        System.out.println("[[ATTACHMENT|" + path.getAbsolutePath() + "]]");
-        System.out.println();
-        
-        
-        //In a syntax like the unit tests
-        System.out.println();
-        System.out.println("[exec] [[ATTACHMENT|" + path + "]]");
-        System.out.println();
-        
-        //all 3 again in std error for good measure
-        
-        //what I would think it would be:
-        System.err.println();
-        System.err.println("[[ATTACHMENT|" + path + "]]"); 
-        System.err.println();
-        
-        //with a stupidly absolute path
-        System.err.println();
-        System.err.println("[[ATTACHMENT|" + path.getAbsolutePath() + "]]");
-        System.err.println();
-        
-        
-        //In a syntax like the unit tests
-        System.err.println();
-        System.err.println("[exec] [[ATTACHMENT|" + path + "]]");
-        System.err.println();
+//        //with a stupidly absolute path
+//        System.out.println();
+//        System.out.println("[[ATTACHMENT|" + path.getAbsolutePath() + "]]");
+//        System.out.println();
+//        
+//        
+//        //In a syntax like the unit tests
+//        System.out.println();
+//        System.out.println("[exec] [[ATTACHMENT|" + path + "]]");
+//        System.out.println();
+//        
+//        //all 3 again in std error for good measure
+//        
+//        //what I would think it would be:
+//        System.err.println();
+//        System.err.println("[[ATTACHMENT|" + path + "]]"); 
+//        System.err.println();
+//        
+//        //with a stupidly absolute path
+//        System.err.println();
+//        System.err.println("[[ATTACHMENT|" + path.getAbsolutePath() + "]]");
+//        System.err.println();
+//        
+//        
+//        //In a syntax like the unit tests
+//        System.err.println();
+//        System.err.println("[exec] [[ATTACHMENT|" + path + "]]");
+//        System.err.println();
         
         
         
