b37871695c37d55a3926458e35fc2c3858e1c801
marklemay <riteofwhey@gmail.com>
2013-12-12 14:43:18 -0500

    perhaps it will build now

--- src/main/stuff/IntentionalyFailingFireFoxTest.java
+++ src/main/stuff/IntentionalyFailingFireFoxTest.java
@@ -3,56 +3,56 @@
  */
 package stuff;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-
-import org.junit.Ignore;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitWebElement;
-
+//import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+//
+//import org.junit.Ignore;
+//import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitWebElement;
+//
 
 /**
  * @author markl
  *
  */
-@Ignore("Ignored for obvious reasons")
-public class IntentionalyFailingFireFoxTest extends IntegrationTest {
+//@Ignore("Ignored for obvious reasons")
+public class IntentionalyFailingFireFoxTest {//extends IntegrationTest {
 
-    /**
-     * @param w
-     */
-    public IntentionalyFailingFireFoxTest() {
-        super(new FirefoxDriver());
-    }
-
-    @CaptureFile String otherDubugInformation = "";
-    String impotantDubugInformation = "";
-
-
-    @CaptureFile
-    public String captureDebug() {
-        return impotantDubugInformation;
-    }
-
-    //    @Test
-    //    public void fails() {
-    //        impotantDubugInformation = "dg is 'fun'";
-    //        otherDubugInformation = "the problem is complicated";
-    //        fail();
-    //    }
-
-    @Test
-    public void failsinBrowser() {
-        driver.get(baseDomain + "/view/product/41580");
-        impotantDubugInformation = "dg is 'fun'";
-
-        HtmlUnitWebElement elem = (HtmlUnitWebElement) waiter.withMessage("regression for #3272").until(
-                presenceOfElementLocated(By.partialLinkText("Title Thingy")));
-    }
-
-
-    @Test
-    public void succeeds() {
-    }
+//    /**
+//     * @param w
+//     */
+//    public IntentionalyFailingFireFoxTest() {
+//        super(new FirefoxDriver());
+//    }
+//
+//    @CaptureFile String otherDubugInformation = "";
+//    String impotantDubugInformation = "";
+//
+//
+//    @CaptureFile
+//    public String captureDebug() {
+//        return impotantDubugInformation;
+//    }
+//
+//    //    @Test
+//    //    public void fails() {
+//    //        impotantDubugInformation = "dg is 'fun'";
+//    //        otherDubugInformation = "the problem is complicated";
+//    //        fail();
+//    //    }
+//
+//    @Test
+//    public void failsinBrowser() {
+//        driver.get(baseDomain + "/view/product/41580");
+//        impotantDubugInformation = "dg is 'fun'";
+//
+//        HtmlUnitWebElement elem = (HtmlUnitWebElement) waiter.withMessage("regression for #3272").until(
+//                presenceOfElementLocated(By.partialLinkText("Title Thingy")));
+//    }
+//
+//
+//    @Test
+//    public void succeeds() {
+//    }
 }
