7c707daa4c891a876bd6d1fb1374ff73cc00f099
marklemay <riteofwhey@gmail.com>
2013-12-19 12:56:35 -0500

    this should run

--- src/test/stuff/openUpFireFoxAtAll.java
+++ src/test/stuff/openUpFireFoxAtAll.java
@@ -1,11 +1,18 @@
 package stuff;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class openUpFireFoxAtAll {
+public class openUpFireFoxAtAll extends IntegrationTest {
 	
-    @Test
+    public openUpFireFoxAtAll() {
+		super(new HtmlUnitDriver(false));
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
 	public void helloGoogle() {
     	FirefoxDriver driver =new FirefoxDriver();
     	driver.get("http://www.google.com/");
